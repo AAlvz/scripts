bb264ea2a9bd1ad25cd9cac56c3a784c64b3e61b
AAlvz <alfonso@alvz.mx>
2014-12-20 16:25:33 +0100

    This enables the use of different keyboard layouts at the same time

---
+++ multiplekeyboards.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#first grep is the name of device, second to check it is a keyboard
+xinput -list | grep -i HID | grep -i keyboard | awk -F= {'print $2'} | awk '{print $1}' |
+xargs -I num setxkbmap -device num -layout us
