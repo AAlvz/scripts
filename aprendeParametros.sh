8cc4281c63c8155657f89bfadc44c8e351ac1ce0
AAlvz <alfonso@alvz.mx>
2015-07-19 15:58:29 -0500

    Added more important commands that can be used in shell

--- aprendeParametros.sh
+++ aprendeParametros.sh
@@ -10,34 +10,34 @@
 #             VARIABLES YOU WILL NEED:
 #
 #
-#    $1, $2 ... $n 
-#       --- To get the incoming parameter by position.
+#    $1, $2 ... $n
+#          --- To get the incoming parameter by position.
 #
-#    $* --- Returns all your parameters in a single string
+#    $*    --- Returns all your parameters in a single string
 #                ("$1, $2 .... $n")
 #
-#    $@ --- Returns all your parameters as a sequence of strings
+#    $@    --- Returns all your parameters as a sequence of strings
 #                ("$1", "$2", ... "$n")
 #
-#    $0 --- Refers to the name of the script itself. 
-#    
-#    $# --- Returns the number of arguments specified via command line
+#    $0    --- Refers to the name of the script itself.
+#
+#    $#    --- Returns the number of arguments specified via command line
 #
 #    $USER --- Returns actual user
 #
-#   
+#    $?    --- Returns last exit status
 ######################################################################
 
 
 #//////////////////////////////////////////////////////////////////////////
 #
-#            ///   CHMOD   /// 
+#            ///   CHMOD   ///
 #
 #        You need to give execute permissions to the file to use it as a script
 #
 #      Run it is simple:
 #                $ chmod [permission] [file]
-#     example    $ chmod 555 scriptname.sh     
+#     example    $ chmod 555 scriptname.sh
 #
 #     The permissions:
 #
@@ -47,7 +47,7 @@
 #             a-x --- Deny execute permissions to everyone
 #
 #           ___________________________________________
-#      W/Letters 
+#      W/Letters
 #      (user+permission)
 #        USER
 #           u   --- User  | The user who owns it
@@ -92,3 +92,46 @@ function how_many {
 
 how_many "$*"
 how_many "$@"
+
+## MAKE SURE TO RUN AS SUDO
+if [[ "$(whoami)" != 'root' ]]
+   then
+   echo "You are not running this as sudo!  D= "
+   exit
+fi
+
+##############
+# ## COLORS! #
+##############
+
+#Will Print Red
+txtrst=$(tput sgr0) # Text reset
+txtred=$(tput setaf 1) # Red
+echo “Welcome to ${txtred} red Text ${txtrst}!”
+
+txtgrn=$(tput setaf 2) # Green
+txtylw=$(tput setaf 3) # Yellow
+txtblu=$(tput setaf 4) # Blue
+txtpur=$(tput setaf 5) # Purple
+txtcyn=$(tput setaf 6) # Cyan
+txtwht=$(tput setaf 7) # White
+
+# tput setab [1-7] : Set a background colour using ANSI escape
+# tput setb [1-7] : Set a background colour
+# tput setaf [1-7] : Set a foreground colour using ANSI escape
+# tput setf [1-7] : Set a foreground colour
+
+# tput Text Mode Capabilities:
+
+# tput bold : Set bold mode
+# tput dim : turn on half-bright mode
+# tput smul : begin underline mode
+# tput rmul : exit underline mode
+# tput rev : Turn on reverse mode
+# tput smso : Enter standout mode (bold on rxvt)
+# tput rmso : Exit standout mode
+# tput sgr0 : Turn off all attributes
+
+##############
+# End Colors #
+##############
